6faf89cee21e75b8ad5c4de6d71dbf817ddda778
unknown <l4wl3ss1989@gmail.com>
2019-08-07 23:36:12 +0200

    Updates

---
+++ precache-manifest.ae06528e5814977ad5ab2e6ffee79b9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2a7dfbc4cc2411d9aea49d04a665a2",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8aeb617d726e8b597854",
+    "url": "/react-monsters-rolodex/static/css/main.7cbfc761.chunk.css"
+  },
+  {
+    "revision": "f5fe11c915f92668079e",
+    "url": "/react-monsters-rolodex/static/js/2.f105d4e9.chunk.js"
+  },
+  {
+    "revision": "8aeb617d726e8b597854",
+    "url": "/react-monsters-rolodex/static/js/main.4cffebe9.chunk.js"
+  },
+  {
+    "revision": "09fc101723e82bb4a8f4",
+    "url": "/react-monsters-rolodex/static/js/runtime~main.2a38a34c.js"
+  }
+]);
